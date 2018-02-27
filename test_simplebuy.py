77ddb57b07e1a45033803fd8d731bd3d103cb2a8
Xuwen <caoxuwen@gmail.com>
2018-02-26 22:45:34 -0800

    three TA agents

--- test_simplebuy.py
+++ test_simplebuy.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 import trading_env
 
+#np.set_printoptions(threshold=np.nan)
+
 #df = pd.read_hdf('dataset/SGXTW.h5', 'STW')
 #df = pd.read_hdf('dataset/SGXTWsample.h5', 'STW')
 df = pd.read_csv('dataset/btc_indexed.csv')
 print(df.describe())
 
-env = trading_env.make(env_id='training_v1', obs_data_len=256, step_len=128,
-                       df=df, fee=0.1, max_position=5, deal_col_name='close', 
+env = trading_env.make(env_id='training_v1', obs_data_len=1, step_len=1,
+                       df=df, fee=0.0, max_position=5, deal_col_name='close', 
                        feature_names=['low', 'high', 
                                       'open','close', 
                                       'volume','datetime'])
@@ -17,14 +19,17 @@ env = trading_env.make(env_id='training_v1', obs_data_len=256, step_len=128,
 env.reset()
 env.render()
 
-state, reward, done, info = env.step(random.randrange(3))
+state, reward, done, info = env.step(2)
+print(state)
+print(state.shape, info)
 
 ### randow choice action and show the transaction detail
-for i in range(200):
-    print(i)
-    state, reward, done, info = env.step(random.randrange(3))
+for i in range(10):
+    state, reward, done, info = env.step(0)
     print(state, reward)
     env.render()
     if done:
         break
-env.transaction_details
+
+state, reward, done, info = env.step(2)
+print(state, reward)
